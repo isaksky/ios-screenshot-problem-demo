b311aa175498d74eb2d47a32846b4736b5aa9653
Isak Sky <isak.sky@gmail.com>
2013-07-08 13:54:54 -0700

    initial commit

---
+++ ScreenshotProblemDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScreenshotProblemDemo
+//
+//  Created by Isak Sky on 7/5/13.
+//  Copyright (c) 2013 YouEye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
